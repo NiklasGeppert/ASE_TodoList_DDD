2a626a33123ef47c3fe1d42ea8f03fe3c8242804
nikla <niklas.gep@gmail.com>
2024-05-01 18:35:00 +0200

    added docker and mongo db

--- plugin/src/main/java/de/dhbw/ase/plugins/persistence/PersonRepositoryBridge.java
+++ plugin/src/main/java/de/dhbw/ase/plugins/persistence/PersonRepositoryBridge.java
@@ -1,9 +1,15 @@
 package de.dhbw.ase.plugins.persistence;
 
 import de.dhbw.ase.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class PersonRepositoryBridge implements PersonRepository {
-    private final PersonRepositoryData
+    private final PersonRepositoryData personRepositoryData;
+
+    @Autowired
+    public PersonRepositoryBridge(final PersonRepositoryData personRepositoryData) {
+        this.personRepositoryData = personRepositoryData;
+    }
 }
--- plugin/src/main/java/de/dhbw/ase/plugins/persistence/PersonRepositoryData.java
+++ plugin/src/main/java/de/dhbw/ase/plugins/persistence/PersonRepositoryData.java
@@ -1,6 +1,10 @@
 package de.dhbw.ase.plugins.persistence;
 
 
+import de.dhbw.ase.entities.Person;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface PersonRepositoryData extends MongoReposi{
+import java.util.UUID;
+
+public interface PersonRepositoryData extends MongoRepository<Person, UUID> {
 }
